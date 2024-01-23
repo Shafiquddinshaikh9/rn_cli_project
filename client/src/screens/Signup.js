b452c850609c4de0ac1123ee11250c2e3a54f074
SHAFIQUDDIN SHAIKH <shafiquddinshaikh9@gmail.com>
2024-01-23 12:29:13 +0530

    signup page updated

--- client/src/screens/Signup.js
+++ client/src/screens/Signup.js
@@ -1,14 +1,54 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
+import {useNavigation} from '@react-navigation/native';
 
 const Signup = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Signup</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Signup</Text>
+      <CustomInput
+        placeholder={'Enter Email ID'}
+        icon={require('../images/mail.png')}
+      />
+      <CustomInput
+        placeholder={'Enter Password'}
+        icon={require('../images/hide.png')}
+        type={'password'}
+      />
+      <CustomButton
+        title={'Signup'}
+        bgColor={'#000'}
+        textColor={'#fff'}
+        onPress={() => {}}
+      />
+      <Text
+        style={styles.signupGo}
+        onPress={() => {
+          navigation.navigate('login');
+        }}>
+        Create new account?
+      </Text>
     </View>
   );
 };
 
 export default Signup;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+  },
+  signupGo: {
+    fontSize: 18,
+    fontWeight: '800',
+    alignSelf: 'center',
+  },
+});
